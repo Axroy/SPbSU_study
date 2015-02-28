34b5730489c441d9aa3427189c70e3345d97d55f
Nikita Smolyakov <smolyakov_na@mail.ru>
2015-02-28 18:31:07 +0300

    Added some introduction for probable user

--- 2nd semester/hw03/02/main.cpp
+++ 2nd semester/hw03/02/main.cpp
@@ -3,7 +3,17 @@
 
 int main()
 {
-    const int size = 4;
+    std::cout << "The program shows elements of a square array going spiral-like from it's center.";
+    std::cout << "So, its size must be an even number.\n";
+    std::cout << "Enter the array's size: ";
+    int size = 0;
+    std::cin >> size;
+    if (size % 2 != 0)
+    {
+        std::cout << "It's an odd number!";
+        return -1;
+    }
+
     SquareArray *array = new SquareArray(size);
     array->print();
     delete array;
