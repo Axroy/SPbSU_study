69c03ef965458e4d1a07fe519acf8cc21999c93e
Nikita Smolyakov <smolyakov_na@mail.ru>
2015-04-09 20:00:10 +0300

    Added documentation.

--- 2nd semester/hw04/02/simpleCalculator.h
+++ 2nd semester/hw04/02/simpleCalculator.h
@@ -1,5 +1,6 @@
 #pragma once
 
+///Enumerable type for operations.
 enum Operation
 {
     plus,
@@ -8,14 +9,19 @@ enum Operation
     divide
 };
 
+///Class for calculating simple integer expressions consisting of two numbers and one operation.
 class SimpleCalculator
 {
 public:
     SimpleCalculator();
 
+    ///Calculates the integer expression. Returns zero in case of dividing by it.
     int getResult();
+    ///Changes first number to some value.
     void changeFirstNumber(int value);
+    ///Changes second number to some value.
     void changeSecondNumber(int value);
+    ///Changes operation to some operation type from certain enumerable.
     void changeOperation(Operation newOperation);
 
 private:
