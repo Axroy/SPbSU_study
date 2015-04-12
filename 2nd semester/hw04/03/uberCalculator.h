64aef8fd1e63100a38dfa814fae57e278b6b2b04
Nikita Smolyakov <smolyakov_na@mail.ru>
2015-04-12 20:48:48 +0300

    Added documentation.

--- 2nd semester/hw04/03/uberCalculator.h
+++ 2nd semester/hw04/03/uberCalculator.h
@@ -1,5 +1,6 @@
 #pragma once
 
+///Enumerable for more convenient use of operations.
 enum Operation
 {
     none,
@@ -9,15 +10,29 @@ enum Operation
     divide
 };
 
+///Class for handling the UberCalculatorUI form.
+///Calculates expressions consisting of two arguments and an operation.
 class UberCalculator
 {
 public:
     UberCalculator();
+
+    ///Changes first argument to certain value.
     void changeFirstArgument(float value);
+
+    ///Changes second argument to certain value.
     void changeSecondArgument(float value);
+
+    ///Changes operation to certain operation type from the enumerable.
     void changeOperation(Operation newOperation);
+
+    ///Returns true if operation type is 'none'.
     bool hasNoOperation();
+
+    ///Calculates the expression and return result.
     float getResult();
+
+    ///Resets arguments and operation to default.
     void reset();
 
 private:
