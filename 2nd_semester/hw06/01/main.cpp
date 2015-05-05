67960f94d1315314be32ea7fa970d99cb3ebd5a1
Nikita Smolyakov <smolyakov_na@mail.ru>
2015-05-05 17:59:22 +0300

    Minor changes to UI.

--- 2nd_semester/hw06/01/main.cpp
+++ 2nd_semester/hw06/01/main.cpp
@@ -10,13 +10,13 @@ int main(int argc, char *argv[])
     QTest::qExec(&tests);
 
     QCoreApplication a(argc, argv);
-    printf("The program for calculating arithmetical expressions using parse tree.\n");
+    std::cout << ("\nThe program for calculating arithmetical expressions using parse tree.\n");
 
     std::string fileName = "";
-    printf("Enter a name of file containing the expression in prefix form:\n");
+    std::cout << ("Enter a name of file containing the expression in prefix form:\n");
     std::cin >> fileName;
 
-    std::cout << Counter::countExpression(fileName);
+    std::cout << "Your result: " << Counter::countExpression(fileName);
 
     return a.exec();
 }
