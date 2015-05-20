89fdc2dc8e275a2de09f65da7391d9079333d4b6
Nikita Smolyakov <smolyakov_na@mail.ru>
2015-05-20 12:57:34 +0300

    Added some documentation.

--- 2nd_semester/hw08/03/ticTacToe.h
+++ 2nd_semester/hw08/03/ticTacToe.h
@@ -1,17 +1,26 @@
 #pragma once
 #include <QPushButton>
 
+///Enumerable for player's turn.
 enum Turn
 {
     cross,
     circle
 };
 
+///Class for checking for draw and win.
 class TicTacToe
 {
 public:
+    ///Gets size of field and length of line needed for victory.
     TicTacToe(int fieldSize, int winLength);
+
+    ///Returns true if vicrory is achieved
+    ///Gets coordinates of button pressed and array of buttons.
     bool checkWin(int coordinateI, int coordinateJ, QPushButton **buttons);
+
+    ///Returns true if draw is achieved.
+    ///Gets array of buttons.
     bool checkDraw(QPushButton **buttons);
 
 private:
