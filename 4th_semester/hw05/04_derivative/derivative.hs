fd882dae15c4f54cac252996c0d958e014e89a88
Nikita Smolyakov <smolyakov_na@mail.ru>
2016-05-15 07:03:56 +0300

    Made it expression, not polynomial.

--- 4th_semester/hw05/04_derivative/derivative.hs
+++ 4th_semester/hw05/04_derivative/derivative.hs
@@ -1,53 +1,59 @@
 import System.IO
 
-data Polynome = X
+data Expression = X
               | Const Int
-              | Polynome :+: Polynome
-              | Polynome :-: Polynome
-              | Polynome :*: Polynome
-              | Polynome :^: Int
+              | Expression :+: Expression
+              | Expression :-: Expression
+              | Expression :*: Expression
+              | Expression :/: Expression
+              | Expression :^: Int
               deriving Show
               
-derive :: Polynome -> Polynome
+derive :: Expression -> Expression
 derive p = case p of
            X -> Const 1
            Const n -> Const 0
            p1 :+: p2 -> derive p1 :+: derive p2
            p1 :-: p2 -> derive p1 :-: derive p2
            p1 :*: p2 -> (derive p1 :*: p2) :+: (p1 :*: derive p2)
+           p1 :/: p2 -> ((derive p1 :*: p2) :-: (p1 :*: derive p2)) :/: (p2 :*: p2)
            p :^: n -> Const n :*: (p :^: (n - 1)) :*: derive p
 
-simplifyMonome :: Polynome -> Polynome
-simplifyMonome m = case m of 
+simplifyMonomial :: Expression -> Expression
+simplifyMonomial m = case m of 
                    Const a :+: Const b -> Const (a + b)
                    Const a :-: Const b -> Const (a - b)
                    Const a :*: Const b -> Const (a * b)
+                   Const a :/: Const b -> Const (a `div` b)
                    Const a :^: b -> Const (a ^ b)
                    
-                   m :+: Const 0 -> simplifyMonome m
-                   m :-: Const 0 -> simplifyMonome m
+                   m :+: Const 0 -> simplifyMonomial m
+                   m :-: Const 0 -> simplifyMonomial m
                    m :*: Const 0 -> Const 0
-                   m :*: Const 1 -> simplifyMonome m 
+                   m :*: Const 1 -> simplifyMonomial m 
+                   m :/: Const 1 -> simplifyMonomial m
                      
-                   Const 0 :+: m -> simplifyMonome m
-                   Const 0 :-: m -> simplifyMonome (Const (-1) :*: simplifyMonome m)
+                   Const 0 :+: m -> simplifyMonomial m
+                   Const 0 :-: m -> simplifyMonomial (Const (-1) :*: simplifyMonomial m)
                    Const 0 :*: m -> Const 0
-                   Const 1 :*: m -> simplifyMonome m
+                   Const 1 :*: m -> simplifyMonomial m
+                   Const 0 :/: m -> Const 0
                      
                    m :^: 0 -> Const 1
-                   m :^: 1 -> simplifyMonome m
+                   m :^: 1 -> simplifyMonomial m
                    
                    _ -> m
            
-simplify :: Polynome -> Polynome
+simplify :: Expression -> Expression
 simplify p = case p of
              X -> X
              Const n -> Const n
  
-             p1 :+: p2 -> simplifyMonome (simplify p1 :+: simplify p2)
-             p1 :-: p2 -> simplifyMonome (simplify p1 :-: simplify p2)
-             p1 :*: p2 -> simplifyMonome (p1 :*: simplify p2)
-             p :^: n -> simplifyMonome (simplify p :^: n)
+             p1 :+: p2 -> simplifyMonomial (simplify p1 :+: simplify p2)
+             p1 :-: p2 -> simplifyMonomial (simplify p1 :-: simplify p2)
+             p1 :*: p2 -> simplifyMonomial (simplify p1 :*: simplify p2)
+             p1 :/: p2 -> simplifyMonomial (simplify p1 :/: simplify p2)
+             p :^: n -> simplifyMonomial (simplify p :^: n)
              
 main = do
         putStrLn (show $ simplify $ derive ((X :^: 3) :+: (Const 3 :*: X) :-: Const 1) )     
