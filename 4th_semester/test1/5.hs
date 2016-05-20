4d5df1cc6b2fc1085307e0b2dcdd04eb9ba64b64
Nikita Smolyakov <smolyakov_na@mail.ru>
2016-05-20 18:28:36 +0300

    Updated the solution to be different.

--- 4th_semester/test1/5.hs
+++ 4th_semester/test1/5.hs
@@ -1,10 +1,12 @@
 rhombus :: Int -> IO ()
-rhombus n = do
-        putStrLn (rhombus_ 1 [])
-        where
-            buildString :: Int -> String
-            buildString pos = '\n' : replicate (n - pos) ' ' ++ replicate (2 * pos - 1) '*'
+rhombus n = rhombus_ 0 n
+            where
+                buildString :: Int -> String
+                buildString pos = '\n' : replicate (n - pos) ' ' ++ replicate (2 * pos - 1) '*'
             
-            rhombus_ :: Int -> String -> String
-            rhombus_ pos rl | pos == n = buildString pos ++ rl
-                            | otherwise = buildString pos ++ rhombus_ (pos + 1) (buildString pos ++ rl)
+                rhombus_ :: Int -> Int -> IO ()
+                rhombus_ pos max | pos <= max = do putStrLn (buildString pos)
+                                                   rhombus_ (pos + 1) max
+                                 | pos <= (2 * max) && pos > max = do putStrLn (buildString (2 * max - pos))
+                                                                      rhombus_ (pos + 1) max
+                                 | otherwise = return ()
